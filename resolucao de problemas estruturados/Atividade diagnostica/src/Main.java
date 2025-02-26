a2cce8ac407144befaf3c4c0923dfb552f7c8eb7
Vinicosper <Cospervini@gmail.com>
2025-02-25 21:58:24 -0300

    inicialização de projeto para revisão

---
+++ resolucao de problemas estruturados/Atividade diagnostica/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente Cliente = new Cliente();
+        Cliente.capturarNome();
+
+
+        System.out.println("Seja bem vindo!\nSelecione o número desejado\n(1)Fazer pedido!\n (2)Sair");
+
+
+    }
+}
